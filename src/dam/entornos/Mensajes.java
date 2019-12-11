1fe8fbee32d113e5744261c434967974ecf16a3c
juanjourbano <58666006+juanjourbano@users.noreply.github.com>
2019-12-11 12:08:19 +0100

    Update Mensajes.java
    Añadido el método alumnoFila4PC4

--- src/dam/entornos/Mensajes.java
+++ src/dam/entornos/Mensajes.java
@@ -15,6 +15,11 @@ public class Mensajes {
 		mensajeProfesor();
 		//
 		// Pon la llamada a tu método a partir de aqui
+		
+		// Este rozo de código no debe modificarse
+		alumnoFila4PC4();
+		//
+		// Pon la llamada a tu método a partir de aqui
 			
 		
 	}
@@ -30,7 +35,14 @@ public class Mensajes {
 	
 	// Pon tu método a partir de aqui
 	
-	
-	
+	// no modifiques este método
+	private static void alumnoFila4PC4() {
+		System.out.println("====================================");
+		System.out.println("Hola, soy Juanjo Urbano Parra (el Juanjo de la última fila).");
+		System.out.println("¡Os deseo a todos un feliz año 2020 y que vuestro stack desborde de felicidad!");
+		System.out.println("====================================");
+		System.out.println();
+	}
+	// Pon tu método a partir de aqui
 	
 }
